f3e341b4e12fd8c76dc0f012a46fb7c167fa5822
lujianan <nihaolujianan@126.com>
2017-11-11 15:59:36 +0800

    array max select

--- 奇技淫巧/js/index.js
+++ 奇技淫巧/js/index.js
@@ -5,3 +5,28 @@ var list = '<ul><li>' + arr.join("</li><li>") + '</li></ul>';
 var dom = document.createElement('div');
 dom.innerHTML = list;
 //document.body.appendChild(dom);
+
+// 句中单词首字母大写,其他小写
+function titleCase(str) { 
+ var string = str.split(' ');
+  for(var i=0;i<string.length;i++){
+    string[i] = string[i][0].toUpperCase() + string[i].substring(1,string[i].length).toLowerCase();
+  }
+  var result =  string.join(" ");
+  return result;
+}
+
+titleCase("I'm a little tea pot");
+
+
+//数组中选取最大值
+function largestOfFour(arr) {
+  var newArr = [],n;
+  for(var i=0;i<arr.length;i++){
+    n = Math.max.apply(null, arr[i]);
+    newArr.push(n);
+  }
+  return newArr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
