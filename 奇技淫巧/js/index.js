bdaedd8667f24c6afa6880c9663554eb73e3511a
lujianan <nihaolujianan@126.com>
2017-12-19 11:53:43 +0800

    css字符省略and金克斯迫击炮

--- 奇技淫巧/js/index.js
+++ 奇技淫巧/js/index.js
@@ -30,3 +30,29 @@ function largestOfFour(arr) {
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+//检查字符串结尾
+function confirmEnding(str, target) { 
+  var re =new RegExp( target + "$","gim");					//正则构造函数，可以传递变量
+  if(re.test(str)){
+    return true;											//如果已指定字符结尾，则返回true
+  }
+  return false;												//否则返回false
+}
+
+confirmEnding("Bastian", "n");
+
+
+//金克斯的迫击炮
+function destroyer(arr) {
+  var args = arguments;										//获取所有参数
+  var arr1 = arr;											//传入参数合并为一个数组，如（1, 2, 3, 1, 2, 3], 2, 3） 变为 （[1, 2, 3, 1, 2, 3, 2, 3]）
+  for (var i = 0; i < args.length; i++) {					//循环参数
+    arr1 = arr1.filter(function (item,index,array){
+      return item !== args[i];								//返回数组中不存在的参数
+    });
+  }
+  return arr1;
+}
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
